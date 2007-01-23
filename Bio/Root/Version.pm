183ffec3067a60c03767c93d0eef78cd627ce025
Sendu Bala <bix@sendu.me.uk>
2007-01-23 11:42:31 +0000

    Import of SENDU/bioperl-1.5.2_101 from CPAN.
    gitpan-cpan-distribution: BioPerl
    gitpan-cpan-version:      1.5.2_101
    gitpan-cpan-path:         SENDU/bioperl-1.5.2_101.tar.gz
    gitpan-cpan-author:       SENDU
    gitpan-cpan-maturity:     developer

--- Bio/Root/Version.pm
+++ Bio/Root/Version.pm
@@ -1,4 +1,4 @@
-# $Id: Version.pm,v 1.9.4.6 2006/12/05 19:23:30 sendu Exp $
+# $Id: Version.pm,v 1.9.4.7 2007/01/23 11:25:49 sendu Exp $
 #
 # BioPerl module for Bio::Root::Version
 #
@@ -85,7 +85,7 @@ methods. Internal methods are usually preceded with a _
 package Bio::Root::Version;
 use strict;
 
-our $VERSION = 1.005002_100;
+our $VERSION = 1.005002_101;
 $VERSION = eval $VERSION;
 
 sub import {
